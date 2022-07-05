73d14574d857bf0d553cff05cc61dacd8b0f3a31
Rishab Mishra <rishab.mishra@bfhl.in>
2022-07-05 09:52:33 +0530

    make task cards clickable

--- src/sections/@dashboard/app/AppWidgetSummary.js
+++ src/sections/@dashboard/app/AppWidgetSummary.js
@@ -1,66 +1,74 @@
 // @mui
 import PropTypes from 'prop-types';
-import { alpha, styled } from '@mui/material/styles';
-import { Card, Typography } from '@mui/material';
+import {alpha, styled} from '@mui/material/styles';
+import {Card, Typography} from '@mui/material';
+import { useNavigate } from "react-router-dom";
 // utils
-import { fShortenNumber } from '../../../utils/formatNumber';
+import {fShortenNumber} from '../../../utils/formatNumber';
 // components
 import Iconify from '../../../components/Iconify';
 
 // ----------------------------------------------------------------------
 
-const IconWrapperStyle = styled('div')(({ theme }) => ({
-  margin: 'auto',
-  display: 'flex',
-  borderRadius: '50%',
-  alignItems: 'center',
-  width: theme.spacing(8),
-  height: theme.spacing(8),
-  justifyContent: 'center',
-  marginBottom: theme.spacing(3),
+const IconWrapperStyle = styled('div')(({theme}) => ({
+    margin: 'auto',
+    display: 'flex',
+    borderRadius: '50%',
+    alignItems: 'center',
+    width: theme.spacing(8),
+    height: theme.spacing(8),
+    justifyContent: 'center',
+    marginBottom: theme.spacing(3),
 }));
 
 // ----------------------------------------------------------------------
 
 AppWidgetSummary.propTypes = {
-  color: PropTypes.string,
-  icon: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  total: PropTypes.number.isRequired,
-  sx: PropTypes.object,
+    color: PropTypes.string,
+    icon: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired,
+    sx: PropTypes.object,
 };
 
-export default function AppWidgetSummary({ title, total, icon, color = 'primary', sx, ...other }) {
-  return (
-    <Card
-      sx={{
-        py: 5,
-        boxShadow: 0,
-        textAlign: 'center',
-        color: (theme) => theme.palette[color].darker,
-        bgcolor: (theme) => theme.palette[color].lighter,
-        ...sx,
-      }}
-      {...other}
-    >
-      <IconWrapperStyle
-        sx={{
-          color: (theme) => theme.palette[color].dark,
-          backgroundImage: (theme) =>
-            `linear-gradient(135deg, ${alpha(theme.palette[color].dark, 0)} 0%, ${alpha(
-              theme.palette[color].dark,
-              0.24
-            )} 100%)`,
-        }}
-      >
-        <Iconify icon={icon} width={24} height={24} />
-      </IconWrapperStyle>
+export default function AppWidgetSummary({title, total, icon, color = 'primary', sx, ...other}) {
+    const navigate = useNavigate();
 
-      <Typography variant="h3">{fShortenNumber(total)}</Typography>
+    function handleClick() {
+        navigate("/dashboard/tasks");
+    }
+    return (
+        <Card
+            sx={{
+                py: 5,
+                boxShadow: 0,
+                textAlign: 'center',
+                color: (theme) => theme.palette[color].darker,
+                bgcolor: (theme) => theme.palette[color].lighter,
+                cursor: 'pointer',
+                ...sx,
+            }}
+            onClick={handleClick}
+            {...other}
+        >
+            <IconWrapperStyle
+                sx={{
+                    color: (theme) => theme.palette[color].dark,
+                    backgroundImage: (theme) =>
+                        `linear-gradient(135deg, ${alpha(theme.palette[color].dark, 0)} 0%, ${alpha(
+                            theme.palette[color].dark,
+                            0.24
+                        )} 100%)`,
+                }}
+            >
+                <Iconify icon={icon} width={24} height={24}/>
+            </IconWrapperStyle>
 
-      <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
-        {title}
-      </Typography>
-    </Card>
-  );
+            <Typography variant="h3">{fShortenNumber(total)}</Typography>
+
+            <Typography variant="subtitle2" sx={{opacity: 0.72}}>
+                {title}
+            </Typography>
+        </Card>
+    );
 }
